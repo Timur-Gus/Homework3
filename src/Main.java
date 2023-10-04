99f86194bfb864b3273b83a49da5815c7aaa11c7
Timur-Gus <timurka24478@gmail.com>
2023-10-04 11:10:42 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //Задание 1
+        int valueInt = 28888888;
+        long valueLong = 99991939113301030L;
+        short valueShort = 30000;
+        byte valueByte = 120;
+        double valueDouble = 12.331413414134444;
+        float valueFloat = 12.47578f;
+
+        System.out.println("Значение переменной valueInt с типом int равно " + valueInt);
+        System.out.println("Значение переменной valueLong с типом long равно " + valueLong);
+        System.out.println("Значение переменной valueShort с типом short равно " + valueShort);
+        System.out.println("Значение переменной valueByte с типом byte равно " + valueByte);
+        System.out.println("Значение переменной valueDouble с типом double равно " + valueDouble);
+        System.out.println("Значение переменной valueFloat с типом float равно " + valueFloat);
+
+
+    }
+}
